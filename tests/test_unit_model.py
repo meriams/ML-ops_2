a5f6187eb9a0d63c81ab56f6c48e217dea154ac0
unknown <eliastsikoss@gmail.com>
2023-11-29 14:47:34 +0100

    test added coverage implemented

--- tests/test_unit_model.py
+++ tests/test_unit_model.py
@@ -1,49 +1,53 @@
 
 import torch
-from torchvision.transforms import RandomHorizontalFlip
-from torch.utils.data import WeightedRandomSampler 
 from sklearn.metrics import classification_report 
 from torchvision.transforms import RandomCrop 
 from torchvision.transforms import Grayscale 
 from torchvision.transforms import ToTensor 
-from torch.utils.data import random_split 
 from torch.utils.data import DataLoader 
-# import config as cfg 
 from torchvision import transforms 
-# from model import EmotionNet 
 from torchvision import datasets 
 import matplotlib.pyplot as plt 
 from collections import Counter 
 from datetime import datetime
-from torch.optim import SGD 
 import torch.nn as nn 
 import pandas as pd 
-import argparse 
-import math 
 import os 
 import hydra
 
 import numpy as np
 import cv2
 import pytest
-from google.cloud import storage
+# from google.cloud import storage not used
 
+
+_TEST_ROOT = os.path.dirname(__file__)  # root of test folder
+_PROJECT_ROOT = os.path.dirname(_TEST_ROOT)  # root of project
+_PATH_DATA = os.path.join(_PROJECT_ROOT, "dataset_local_git")  # root of data
+
+print("test,project,data",_TEST_ROOT,_PROJECT_ROOT,_PATH_DATA) 
+#local data path
+#import tests.path_data
+#from . import _PATH_DATA
+#_PATH_DATA
+#pathdata
 print('Imports ok')
 
 ###################################################################
 PROJECT_DIR = os.environ.get("GITHUB_WORKSPACE", None)
+print("project dir",PROJECT_DIR) # None
 
 # Checking Input Shape of Train Images
 # def test_input_image_shape():
-img = cv2.imread(os.path.join(PROJECT_DIR, "data/raw/train/angry/Training_3908.jpg"))
-# img = cv2.imread("data/raw/train/angry/Training_3908.jpg")
+
+img = cv2.imread(os.path.join(_PATH_DATA, "train/angry/Training_3908.jpg"))
 train_height, train_width, train_channels = img.shape
 print(' -> Train image shape', train_height, train_width, train_channels)
 
 
 # Checking Input Shape of Test Images
 # def test_input_image_shape():
-img = cv2.imread(os.path.join(PROJECT_DIR, "data/raw/test/angry/PrivateTest_88305.jpg"))
+img = cv2.imread(os.path.join(_PATH_DATA, "test/angry/PrivateTest_88305.jpg"))
 # img = cv2.imread("data/raw/test/angry/PrivateTest_88305.jpg")
 test_height, test_width, test_channels = img.shape
 print (' -> Test image shape', test_height, test_width, test_channels)
@@ -67,9 +71,9 @@ def train_transformer():
 
 current_file_dir = os.path.dirname(os.path.abspath(__file__))
 # Traverse two directories up from the current file's directory
-ROOT_DIR = os.path.abspath(os.path.join(current_file_dir, '..'))
-DATASET_FOLDER = os.path.join(ROOT_DIR, "data")
-trainDirectory = os.path.join(DATASET_FOLDER, "raw/train")
+#ROOT_DIR = os.path.abspath(os.path.join(current_file_dir, '..'))
+#ET_FOLDER = os.path.join(_PATH_DATA)
+trainDirectory = os.path.join(_PATH_DATA, "train")
 
 train_data = datasets.ImageFolder(trainDirectory, transform=train_transformer)
 print(type(train_data))
@@ -123,5 +127,30 @@ def rules():
     test_width == 48,
     test_channels == 3]
 
+@pytest.fixture
+def weight_rules():
+    t1 = torch.tensor([7., 7., 7., 7., 7., 7., 7.])
+    return t1.numpy()
+
+def test_class_weight(weight_rules):
+    class_count = Counter(train_data.classes)
+    print(f'[INFO] Total sample: {class_count}')
+    # depending on the number of samples available 
+    class_weight = torch.Tensor([len(train_data.classes) / c
+                                for c in pd.Series(class_count).sort_index().values])
+    assert class_weight.numpy().all() == weight_rules.all()
+  
+class_count = Counter(train_data.classes)
+    # depending on the number of samples available 
+class_weight = torch.Tensor([len(train_data.classes) / c
+                                for c in pd.Series(class_count).sort_index().values])  
+print(class_weight)
 
 
+#             # count each labels within each classes 
+# class_count = Counter(train_data.classes)
+# print(f'[INFO] Total sample: {class_count}')
+#             # depending on the number of samples available 
+# class_weight = torch.Tensor([len(train_data.classes) / c
+#                                         for c in pd.Series(class_count).sort_index().values])
+# print(type(class_weight),class_weight)
