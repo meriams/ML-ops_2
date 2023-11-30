780997c9ba5b0c68e348f8cc3b93483a40945019
Crimson Scythe <crimsonscythestudios@gmail.com>
2023-11-30 22:35:05 +0100

    added opentelemry stuff only works locally

--- src/main.py
+++ src/main.py
@@ -1,7 +1,6 @@
 '''
 Create a Fastapi app with a single POST endpoint. It should be possible to upload an image file. The image file is used as input to a vision model. The model should be run in inference mode and the api call should return the model output. 
 '''
-
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
@@ -21,9 +20,25 @@ from models import EmotionNet
 from PIL import Image
 import io
 from torchvision.transforms import Compose, Grayscale, ToTensor
+''' Open telemerty stuff (only works locally) '''
+# from opentelemetry import trace
+# from opentelemetry.exporter.otlp.proto.http.trace_exporter import (
+#     OTLPSpanExporter,
+# )
+# from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+# from opentelemetry.sdk.trace import TracerProvider
+# from opentelemetry.sdk.trace.export import BatchSpanProcessor
 
+# # set up tracing and open telemetry
+# provider = TracerProvider()
+# processor = BatchSpanProcessor(OTLPSpanExporter())
+# provider.add_span_processor(processor)
+# trace.set_tracer_provider(provider)
+# tracer = trace.get_tracer(__name__)
 
 app = FastAPI()
+''' Open telemerty stuff (only works locally) '''
+# FastAPIInstrumentor.instrument_app(app)
 
 @app.get("/")
 async def root():
@@ -37,6 +52,12 @@ async def model(data: UploadFile = File(...)):
     model_pth = os.path.join(mnt_dir, "my_model.pth")
     print("this is the dir")
     print(model_pth)
+
+   # get the current span
+    ''' Open telemerty stuff (only works locally) '''
+    # current_span = trace.get_current_span()
+
+    # model_pth = "/Users/kamal/Documents/ML-ops_2/src/models/models/my_model.pth"
     model = EmotionNet(num_of_channels=1, num_of_classes=7)
     model.load_state_dict(torch.load(f=model_pth, map_location=torch.device('cpu')))
     model = model.to("cpu")
@@ -60,6 +81,10 @@ async def model(data: UploadFile = File(...)):
     res_mapping = {"0": 'angry', "1": 'disgust', "2": 'fear', "3": 'happy', "4": 'neutral', "5": 'sad', "6": 'surprise'}
     index = torch.argmax(torch.nn.functional.softmax(out, dim=1)).detach().numpy().item()
     
+    ''' Open telemerty stuff (only works locally) '''
+        # save some features to opentelemetry
+    # current_span.set_attribute("output", res_mapping[str(index)])  # <- Saves attribute
+    # current_span.set_attribute("label_probability", torch.nn.functional.softmax(out, dim=1).detach().numpy().item())
     response = {
         "output": res_mapping[str(index)],
         "message": "success",
